caccc771979087b747e060fccd6f9e504f59b400
girlprog <f.galaudu@gmail.com>
2020-08-15 00:36:33 +0100

    print store alert fix

--- salaryapp/views.py
+++ salaryapp/views.py
@@ -9,6 +9,8 @@ import imaplib
 import email
 from email import policy
 from email.parser import BytesParser
+from html.parser import HTMLParser
+
 
 ORG_EMAIL   = "@yedi.com.ng"
 FROM_EMAIL  = "ps" + ORG_EMAIL
@@ -30,29 +32,39 @@ def ps_email(request):
 
 
         for num in data[0].split()[::-1] :
-            if counter < 10:
+            if counter < 19:
                 print(counter)
                 typ, data = mail.fetch(num, '(RFC822)')
                 # print ('Message %s\n%s\n' % (num, data[0][1]))
-                alert = data[0][1].split(b"\r\nSubject", 1)[1].split(b"\r\nDate:", 1)[0]
+                # alert = data[0][1].split(b"\r\nSubject", 1)[1].split(b"\r\nDate:", 1)[0]
+                alert = email.message_from_bytes(data[0][1].split(b"\r\nSubject", 1)[1].split(b"\r\nDate:", 1)[1]).as_string()
+                remark = alert.split('Remarks')[1].split('Time of Transaction')[0].strip().replace('=20', '', 20)
+                amount = alert.split('Amount')[1].split('Value Date')[0].strip().replace('=20', '', 20)
+                date = alert.split('Value Date')[1].split('Remarks')[0].strip().replace('=20', '', 20)
 
-                with open(data[0][1], 'rb') as fp:
-                    msg = BytesParser(policy=policy.default).parse(fp)
+
+
+                # with open(data[0][1], 'rb') as fp:
+                #     msg = BytesParser(policy=policy.default).parse(fp)
 
                 # Now the header items can be accessed as a dictionary, and any non-ASCII will
                 # be converted to unicode:
-                    print('To:', msg['to'])
-                    print('From:', msg['from'])
-                    print('Subject:', msg['subject'])
+                #     print('To:', msg['to'])
+                #     print('From:', msg['from'])
+                #     print('Subject:', msg['subject'])
 
                 # sender = data[0][1].split(b"ransfer from", 1)[0].split(b"to", 1)[0]
                 # return HttpResponse(data[0][1].split(b"\r\nSubject", 1)[1].split(b"to PRINTSTORE", 1)[0])
                 if 'Credit' in str(alert) and 'REVERSAL' not in str(alert):
-                    msgs.append(str(alert).replace("b': ", '').replace("'", '') + '\r\n')
+                    # msgs.append(str(alert).replace("b': ", '').replace("'", '') + '\r\n')
+                    # msgs.append(remark)
+                    # msgs.append(amount)
+                    msgs.append(f'{date} - ₦{amount} <br> {remark} <br><br>')
                     counter = counter + 1
         mail.close()
         mail.logout()
-        html = "<html><body> <h1> Print Store Alerts: </h1> <br> <h2> %s. </h2></body></html>" % ' '.join(msgs).replace('\r\n', '<br>')
+        # html = "<html><body> <h1> Print Store Alerts: </h1> <br> <h2> %s. </h2></body></html>" % ' '.join(msgs).replace('\r\n', '<br>')
+        html = "<html><body> <h1> Print Store Alerts: </h1> <br> <h2> %s. </h2></body></html>" % ' '.join(msgs).replace(':', '')
         return HttpResponse(html)
 
         # type, data = mail.search(None, 'ALL')
